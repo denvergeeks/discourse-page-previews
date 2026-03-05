1cf6531b2dbc445a4ca76e0edfc2b3b3ab5c22a7
denvergeeks <jen@denvergeeks.com>
2026-03-05 02:47:13 -0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,54 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
-# required_version: 2.7.0
+# name: discourse-page-previews
+# about: Adds hover previews for pages and posts with Ctrl+hover support
+# version: 1.0.0
+# authors: Jen Story
+# url: https://github.com/yourusername/discourse-page-previews
+# required_version: 3.1.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :page_previews_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
-end
-
-require_relative "lib/my_plugin_module/engine"
+register_asset "stylesheets/page-previews.scss"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  module ::DiscoursePagePreviews
+    PLUGIN_NAME = "discourse-page-previews"
+
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscoursePagePreviews
+    end
+  end
+
+  require_relative "app/controllers/page_previews_controller"
+  require_relative "app/serializers/page_preview_serializer"
+
+  DiscoursePagePreviews::Engine.routes.draw do
+    get "/page-previews/:id" => "page_previews#show", constraints: { id: /\d+/ }
+  end
+
+  Discourse::Application.routes.append do
+    mount ::DiscoursePagePreviews::Engine, at: "/"
+  end
+
+  # Add guardian method for preview access
+  add_to_class(:guardian, :can_preview_page?) do |page|
+    return false unless SiteSetting.page_previews_enabled
+    return false unless authenticated?
+    return false if page.blank?
+    return false unless page.respond_to?(:topic)
+    
+    # Check if user can see the topic
+    can_see_topic?(page.topic)
+  end
+
+  add_to_class(:guardian, :can_preview_post?) do |post|
+    return false unless SiteSetting.page_previews_enabled
+    return false unless authenticated?
+    return false if post.blank?
+    
+    # Check if user can see the post
+    can_see_post?(post)
+  end
 end
